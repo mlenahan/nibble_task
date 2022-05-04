6cfb0f232485bc595111064c99703377d607d449
Michael Lenahan <michael_lenahan@hotmail.com>
2022-05-04 11:58:56 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,12 @@
+def add_tuple_lists(list_a, list_b):
+    """ Add together two lists of tuples of integers.
+
+    Keyword arguments:
+    list_a -- a list of tuples of integers, all of which have the same length
+    list_b -- another list of tuples of integers, which have the same length as list_a
+
+    Returns:
+    a single list of tuples, each one being the sum of the equivalent tuples in list a and list b
+    """
+
+    # Oh dear, there's nothing here!
