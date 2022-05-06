09b7104db8a2284bfb379640b98fd08d9d913ce5
Michael Lenahan <michael_lenahan@hotmail.com>
2022-05-06 13:35:50 +0000

    solution to problem implemented

--- project.py
+++ project.py
@@ -8,92 +8,10 @@ def add_tuple_lists(list_a, list_b):
     Returns:
     a single list of tuples, each one being the sum of the equivalent tuples in list a and list b
     """
-
-    # two lists of lists
-    # both lists have same length
-    # each inner tuple has the same length
-
-    # list_a = [(1,)]
-    # list_b = [(1,)]
-    # result = [(2,)]
-
-    # list_a = [(1, 1)]
-    # list_b = [(1, 2)]
-    # result = [(2, 3)]
-
-    # list_a = [(1, 1), (1, 1)]
-    # list_b = [(1, 2), (2, 1)]
-    # result = [(2, 3), (3, 2)]
-
-    # take first tuple from list a
-    # take first tuple from list b
-    # take first value from first tuple
-    # take first value from second tuple
-    # sum them
-    # add then to a new tuple
-    # append to output list
-    
-    first_tuple_a = list_a[0]
-    first_tuple_b = list_b[0]
-    first_value_tuple_a = first_tuple_a[0]
-    first_value_tuple_b = first_tuple_b[0]
-    sum_of_values = first_value_tuple_a + first_value_tuple_b
-    new_tuple = tuple(sum_of_values)
-    output_list = []
-    output_list.append(new_tuple)
-    return output_list
-
-    # take first tuple from list a
-    # take first tuple from list b
-    # take first value from first tuple
-    # take first value from second tuple
-    # sum them
-    # add then to a new tuple
-    # take second value from first tuple
-    # take second value from second tuple
-    # sum them
-    # add then to a new tuple
-    # append to output list
-
-    first_tuple_a = list_a[0]
-    first_tuple_b = list_b[0]
-
-    length_of_tuple = len(first_tuple_a)
-
-    sums = []
-
-    for i in range(length_of_tuple):
-        value_tuple_a = first_tuple_a[i]
-        value_tuple_b = first_tuple_b[i]
-        sum_of_values = value_tuple_a + value_tuple_b
-        sums.append(sum_of_values)
-
-    new_tuple = tuple(sums)
-
-    output_list = []
-    output_list.append(new_tuple)
-    return output_list
-
-    # take first tuple from list a
-    # take first tuple from list b
-    # take first value from first tuple
-    # take first value from second tuple
-    # sum them
-    # add then to a new tuple
-    # take second value from first tuple
-    # take second value from second tuple
-    # sum them
-    # add then to a new tuple
-    # append to output list
-    # take second tuple from list a
-    # take second tuple from list b
-    # take first value from first tuple
-    # take first value from second tuple
-    # sum them
-    # add then to a new tuple
-    # take second value from first tuple
-    # take second value from second tuple
-    # sum them
-    # add then to a new tuple
-    # append to output list
-
+    result = []
+    for i in range(len(list_a)):
+        sum_tup = []
+        for j in range(len(list_a[0])):
+            sum_tup.append(list_a[i][j] + list_b[i][j])
+        result.append(tuple(sum_tup))
+    return result
